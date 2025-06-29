c8f28282e557e4cdbc9b953133757311f3863886
Marat <marnerr@live.com>
2025-06-29 19:31:06 +0300

    add index.html

--- main/views.py
+++ main/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Marat')
+    context = {
+        'title': 'HOME - Главная',
+        'content': 'Главная страница магазина - HOME',
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About us')
+    context = {
+        'title': 'HOME - Главная',
+        'content': 'О нас',
+        'text_on_page': '"Замечательный магазин! Всегда нахожу здесь то, что нужно. Вежливые и грамотные консультанты, которые всегда помогут с выбором. Очень радует широкий ассортимент и быстрая доставка. Спасибо за вашу работу!"'
+    }
+    return render(request, 'main/about.html', context)
